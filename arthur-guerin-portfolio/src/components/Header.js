c073f75b26f4f26bb7027ed63e23845547a212ea
Arthuros727 <arthur.guerin@epitech.eu>
2024-08-24 02:41:21 +0200

    ADDlink

--- arthur-guerin-portfolio/src/components/Header.js
+++ arthur-guerin-portfolio/src/components/Header.js
@@ -10,11 +10,11 @@ const Header = () => {
           <h1>Arthur Guerin</h1>
         </div>
         <div className="right-section">
-          <a href="mailto:tonemail@example.com">
+          <a href="mailto:arthur.guerin@epitech.eu">
             <img src='mail.png' alt="Mail" />
           </a>
           <div className="divider"></div>
-          <a href="https://github.com/tonprofil" target="_blank" rel="noopener noreferrer">
+          <a href="https://github.com/Arthuros727" target="_blank" rel="noopener noreferrer">
             <img src="githubo.png" alt="GitHub" />
           </a>
         </div>
