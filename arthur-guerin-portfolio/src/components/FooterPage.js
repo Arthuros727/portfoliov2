f0bfcf239d69e03399c9b3981b61e2f71dfbaed6
Arthuros727 <arthur.guerin@epitech.eu>
2024-08-26 01:24:29 +0200

    [ADD]ajout du backend, des comentaire, du responsive

--- arthur-guerin-portfolio/src/components/FooterPage.js
+++ arthur-guerin-portfolio/src/components/FooterPage.js
@@ -1,15 +1,67 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './FooterPage.css';
-import { FaDownload } from 'react-icons/fa'; // Assurez-vous d'avoir installé react-icons
 
-const FooterPage = () => {
+const FooterPage = ({ commentsRef }) => {
+  const [comments, setComments] = useState([]); 
+  const [newComment, setNewComment] = useState(''); 
+
+//récupérer les commentaires depuis l'API
+  const fetchComments = async () => {
+    try {
+      const response = await fetch('https://arthur-guerin.dev/get_comments.php');
+      const data = await response.json();
+      setComments(data);
+    } catch (error) {
+      console.error('Erreur lors de la récupération des commentaires:', error);
+    }
+  };
+
+  // envoi du formulaire
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (newComment.trim() === '') return; 
+
+    try {
+      const response = await fetch('https://arthur-guerin.dev/submit-comment.php', { 
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ comment: newComment }),
+      });
+
+      if (response.ok) {
+        setNewComment(''); // Efface le champ de saisie
+        fetchComments(); // Recharge les commentaires après l'ajout d'un nouveau commentaire
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'envoi du commentaire:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchComments(); // Charge les commentaires à l'initialisation
+  }, []);
+
   return (
-    <div className="footer-container">
-      <a href="/path/to/your/resume.pdf" className="footer-button" download>
-        <FaDownload className="footer-icon" />
-        Télécharger CV
-      </a>
+    <div className="footer-container" ref={commentsRef}>
+    <p id='commentaire'>Laissez un commentaire !</p>
+      <form className="comment-form" onSubmit={handleSubmit}>
+        <textarea
+          className="comment-textarea"
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="Écrivez votre commentaire ici..."
+        />
+      </form>
+        <button type="submit" className="submit-button">Envoyer</button>
       
+      <div className="comments-list">
+        {comments.map((comment, index) => (
+          <div key={index} className="comment-item">
+            {comment.data}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
